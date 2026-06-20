e07cd24c57b0af1d123b4bee1bf8032f1d6978e6
flashjaysan <flashjaysan@gmail.com>
2026-06-20 10:37:21 +0200

    E01 setting velocity property from direction and move_speed

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -5,5 +5,8 @@ var move_speed: float = 100.0
 
 func _process(delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
+	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	velocity = direction * move_speed
