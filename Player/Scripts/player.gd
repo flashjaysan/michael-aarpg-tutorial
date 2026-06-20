2a891e865b265230c3fc92158f077b7f5afc2fec
flashjaysan <flashjaysan@gmail.com>
2026-06-20 10:36:24 +0200

    E01 modified direction according to inputs

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -5,3 +5,5 @@ var move_speed: float = 100.0
 
 func _process(delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
